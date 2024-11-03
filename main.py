9a03768904c743752946983c891702ad17be034c
sivamani2943 <2100039060cse@gmail.com>
2024-11-04 00:14:43 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.winequality import logger
 from src.winequality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.winequality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -13,4 +14,12 @@ except Exception as e:
         raise e
 
 
-
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
